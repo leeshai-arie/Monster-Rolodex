ce5afc436f5d40dcfc55330286989a346c59b5fe
leeshai-arie <leeshaiarie@gmail.com>
2022-08-20 14:11:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <p>Test</p>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+          id: 1,
+        },
+        {
+          name: 'Frank',
+          id: 2,
+        },
+        {
+          name: 'Jacky',
+          id: 3,
+        },
+        {
+          name: 'Andrei',
+          id: 4,
+        },
+      ]
+    };
+  }
+  
+  render() {
+    return(
+      <div>
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )})}
+      </div>
+    )
+  };
+};
 
 export default App;
